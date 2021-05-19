fd846c6a7a25868c767f18e1dc1f57fab7f8e746
Ajurgs <ajjurgs5954@gmail.com>
2021-05-18 23:12:48 -0600

    fixed the sendfile calls

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const PORT = 3000;
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname + "/public"), "notes.html")
-);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public", "notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public", "index.html"));
+});
 
 app.get("/api/notes", (req, res) => {});
 
 app.post("/api/notes", (res, req) => {});
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname + "/public"), "index.html")
-);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
