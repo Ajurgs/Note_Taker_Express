aec3d8a559c4d7ea44ac3622d03a551899d7424a
Ajurgs <ajjurgs5954@gmail.com>
2021-05-19 18:45:19 -0600

    added the api get function

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +15,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public", "index.html"));
 });
 
-app.get("/api/notes", (req, res) => {});
+app.get("/api/notes", (req, res) => {
+  res.json(path.join(__dirname + "/db", "db.json"));
+});
 
 app.post("/api/notes", (res, req) => {});
 
